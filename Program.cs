bbcc0a45d41538579ace7d0fa97714df5836a116
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-08 22:38:51 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongágua");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
